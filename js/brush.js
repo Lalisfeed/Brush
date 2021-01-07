7443d75d1afbac9972ea43e7d2cb262f053ce0f9
Lali Akhil Raj <AKHILRAJ1729@GMAIL.COM>
2021-01-07 14:32:56 +0530

    painting

--- js/brush.js
+++ js/brush.js
@@ -43,10 +43,27 @@ function Sketch(e) {
     brush.moveTo(e.clientX, e.clientY);
 }
 
+let out = {x:0, y:0};
+
+function painting(e) {
+    painting = true;
+    brush.lineWidth = 10;
+    brush.lineCap = "round";
+    brush.strokeStyle = brushColor
+     if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        out.x = touch.pageX;
+        out.y = touch.pageY;
+      }
+    brush.lineTo(out.x, out.y);
+    brush.stroke();
+    brush.beginPath();
+    brush.moveTo(out.x, out.y);
+}
 
 board.addEventListener('mousedown', StartLocation)
 board.addEventListener('mouseup', EndLocation)
 board.addEventListener('mousemove', Sketch)
-board.addEventListener('touchstart', StartLocation)
-board.addEventListener('touchmove', StartLocation)
+board.addEventListener('touchstart', painting)
+board.addEventListener('touchmove', painting)
 board.addEventListener('touchend', EndLocation)
